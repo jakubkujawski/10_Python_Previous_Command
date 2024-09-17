47aa3ff371102ee405698939fe94f5fa39f518ca
Jakub Kujawski <jaahcob@gmail.com>
2024-09-17 22:34:30 +0100

    Add files via upload

---
+++ 10_Python_Previous_Command.py
@@ -0,0 +1,8 @@
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 2 + + 9
+>>> x = 2 + + 9 # I can quicly repeat the same code by pressing ctrl + p
+>>> 
+>>> # To change it, press IDLE -> Preferences -> Keys -> history-previous <Control-Key-p>
+>>> 
+>>> 
